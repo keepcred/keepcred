5cd11120f0743bff6d9b4f6e79e9c6cc11093eac
Yakhin Nikita Artemovich <n1k@the-dev.ru>
2026-01-03 15:01:57 +0500

    #20 ⚙️ chore: добавлены правила importX
    - Никаких дубликатов;
    - Никаких циклических зависимостей;
    - Никаких абсолютных путей;
    - Никаких изменяемых импортов;
    - Не допускаются неиспользуемые импорты;
    - Импорты идут всегда первыми;
    - Импорты по умолчанию всегда соответствуют своим именам;
    - После импортов всегда идёт отступ;
    - Настроен порядок импортов;
    - Между группами импортов добавлены отступы;
    - Создана отдельная группа subpath испортов;

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,40 @@
 import { defineConfig } from 'eslint/config'
+import importX from 'eslint-plugin-import-x'
 
 /**
  * Конфигурация Eslint
  */
-const config = defineConfig({})
+const config = defineConfig({
+  files: ['./src/**/*.{js,ts,jsx,tsx,mjs,mts,cts,cjs}'],
+  plugins: { importX },
+  rules: {
+    'importX/first': 2,
+    'importX/export': 2,
+    'importX/no-cycle': 2,
+    'importX/no-duplicates': 2,
+    'importX/no-absolute-path': 2,
+    'importX/no-unused-modules': 2,
+    'importX/no-rename-default': 2,
+    'importX/no-mutable-exports': 2,
+    'importX/no-named-as-default': 2,
+    'importX/newline-after-import': 2,
+    'importX/consistent-type-specifier-style': 2,
+    'importX/order': [
+      2,
+      {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type', 'unknown'],
+        'pathGroups': [
+          {
+            group: 'internal',
+            pattern: '#/**',
+            position: 'after',
+          },
+        ],
+        'distinctGroup': true,
+        'newlines-between': 'always',
+      },
+    ],
+  },
+})
 
 export default config
