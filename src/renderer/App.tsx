424a80551d42f6a7ca189775374a198bcf940566
Yakhin Nikita Artemovich <n1k@the-dev.ru>
2026-01-05 22:33:39 +0500

    #105 ✨feat: tRPC подключен к клиенту

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,19 @@
-export function App(): React.JSX.Element {
-  return <></>
+import { useSignals, useSignal } from '@preact/signals-react/runtime'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+import { tRpcClient, tRpcClientReact } from './trpc'
+
+export const App = () => {
+  useSignals()
+
+  const client = useSignal(tRpcClient)
+  const clientTanstack = useSignal(new QueryClient())
+
+  return (
+    <>
+      <tRpcClientReact.Provider client={client.value} queryClient={clientTanstack.value}>
+        <QueryClientProvider client={clientTanstack.value}></QueryClientProvider>
+      </tRpcClientReact.Provider>
+    </>
+  )
 }
