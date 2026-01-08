d261541c7835705cf1a9c1800a90b08b7db057b8
Yakhin Nikita Artemovich <n1k@the-dev.ru>
2026-01-08 22:02:05 +0500

    #133 ✨ feat: добавлены константы криптографии

---
+++ src/main/constants/constant-crypto.ts
@@ -0,0 +1,28 @@
+/**
+ * Длина ключа
+ */
+export const KEY_LENGTH = 32
+/**
+ * Длина соли
+ */
+export const SALT_LENGTH = 16
+/**
+ * Формат преобразования в строку
+ */
+export const FORMAT_HEX = 'hex'
+/**
+ * Алгоритм шифрования
+ */
+export const ALGORITHM = 'aes-256-gcm'
+/**
+ * IV длина
+ */
+export const IV_LENGTH = 12
+/**
+ * KDF формат
+ */
+export const KDF_DIGEST = 'sha256'
+/**
+ * Количество итераций KDF
+ */
+export const KDF_ITERATIONS = 100000
